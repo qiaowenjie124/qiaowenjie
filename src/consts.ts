d95fc5a1d845d20554984c78313cc3d21cf6fa95
qiaowenjie <15397397769@163.com>
2026-05-13 11:10:31 +0800

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -8,9 +8,9 @@ export const BLOG_SLUG_KEY = process.env.BLOG_SLUG_KEY || ''
  * GitHub 仓库配置
  */
 export const GITHUB_CONFIG = {
-	OWNER: process.env.NEXT_PUBLIC_GITHUB_OWNER || 'yysuni',
-	REPO: process.env.NEXT_PUBLIC_GITHUB_REPO || '2025-blog-public',
-	BRANCH: process.env.NEXT_PUBLIC_GITHUB_BRANCH || 'main',
-	APP_ID: process.env.NEXT_PUBLIC_GITHUB_APP_ID || '-',
-	ENCRYPT_KEY: process.env.NEXT_PUBLIC_GITHUB_ENCRYPT_KEY || 'wudishiduomejimo',
+    OWNER: process.env.NEXT_PUBLIC_GITHUB_OWNER || 'qiaowenjie124',
+    REPO: process.env.NEXT_PUBLIC_GITHUB_REPO || 'qiaowenjie',
+    BRANCH: process.env.NEXT_PUBLIC_GITHUB_BRANCH || 'main',
+    APP_ID: process.env.NEXT_PUBLIC_GITHUB_APP_ID || '3652997',
+    ENCRYPT_KEY: process.env.NEXT_PUBLIC_GITHUB_ENCRYPT_KEY || '',
 } as const
